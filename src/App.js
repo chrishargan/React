6d5e833680b67bdb34b45d5c6f54575973834904
chrishargan <chrishargan@gmail.com>
2020-10-19 18:23:16 +0200

    learning about components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hey, I'm a React App</h1>
+                <p>This really works!</p>
+                <Person name="Chris" age="31"/>
+                <Person name="Kevin" age="45">My Hobbies: Sailing</Person>
+                <Person name="Max" age="28"/>
+            </div>
+        );
+        //return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Hi,I\'m a React App'));
+    }
 }
 
 export default App;
