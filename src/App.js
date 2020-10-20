56c3f8ac1090b44cc9ad9694795ccba756a2ea95
chrishargan <chrishargan@gmail.com>
2020-10-20 16:34:38 +0200

    working with states

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hey, I'm a React App</h1>
-                <p>This really works!</p>
-                <Person name="Chris" age="31"/>
-                <Person name="Kevin" age="45">My Hobbies: Sailing</Person>
-                <Person name="Max" age="28"/>
-            </div>
-        );
-        //return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Hi,I\'m a React App'));
-    }
+const app = props => {
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            {name: "Chris", age: 31},
+            {name: "Kevin", age: 45},
+            {name: "Max", age: 28},
+        ]
+    });
+
+    const switchNameHandler = () => {
+        //console.log('was clicked');
+        setPersonsState({
+            persons: [
+                {name: "Christopher", age: 31},
+                {name: "Kevin Karel", age: 45},
+                {name: "Maximillian", age: 28},
+            ]
+        });
+    };
+
+
+    return (
+        <div className="App">
+            <h1>Hey, I'm a React App</h1>
+            <p>This really works!</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Sailing</Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        </div>
+    );
+    //return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Hi,I\'m a React App'));
 }
 
-export default App;
+
+export default app;
+
