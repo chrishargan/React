0ed281f919e9c877389d675b08e5b0b3225cbfad
chrishargan <chrishargan@gmail.com>
2020-10-21 15:51:36 +0200

    working with styling and state change

--- src/App.js
+++ src/App.js
@@ -1,41 +1,77 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person';
 
-const app = props => {
-    const [personsState, setPersonsState] = useState({
+
+class App extends Component {
+    state = {
         persons: [
-            {name: "Chris", age: 31},
-            {name: "Kevin", age: 45},
-            {name: "Max", age: 28},
-        ]
-    });
-
-    const switchNameHandler = () => {
-        //console.log('was clicked');
-        setPersonsState({
-            persons: [
-                {name: "Christopher", age: 31},
-                {name: "Kevin Karel", age: 45},
-                {name: "Maximillian", age: 28},
-            ]
-        });
+            {name: 'Max', age: 28},
+            {name: 'Chris', age: 31},
+            {name: 'Kevin', age: 45}
+        ],
+        otherState: 'some other value'
     };
 
+    switchNameHandler = (newName) => {
+        // console.log('Was clicked!');
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+        this.setState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: 'Christopher', age: 31},
+                {name: 'Kevin', age: 45}
+            ]
+        })
+    };
 
-    return (
-        <div className="App">
-            <h1>Hey, I'm a React App</h1>
-            <p>This really works!</p>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Sailing</Person>
-            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-        </div>
-    );
-    //return React.createElement('div', {className: 'App'},React.createElement('h1',null, 'Hi,I\'m a React App'));
+    nameChangeHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Maximilian', age: 28},
+                {name: 'Christopher', age: 31},
+                {name: event.target.value, age: 45}
+            ]
+        });
+    }
+
+    render() {
+        const styling = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer',
+        };
+        {/* This is an example of how to use inline styling for react to scope the changes only locally   */}
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <p>This is really working!</p>
+                <button
+                    style={styling}
+                    onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+                {/*  this way of state change is when you place the new entry in as parameter into the method when you use an arrow function where you have pointed to the variable that will change i.e newName   */}
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Maximus')}
+                >
+                    {/* another BETTER way is to use the bind to attach the new output    */}
+                    My Hobbies: Racing
+                </Person>
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    changed={this.nameChangeHandler}/>
+            </div>
+        );
+        // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
 }
 
-
-export default app;
-
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Person.css';
+
+{/* Make sure to import using this kind of pathway as the autocomplete version does not find it  */}
 
 const person = (props) => {
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
